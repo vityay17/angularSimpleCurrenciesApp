27282a869b7c41ceb1e15d43a8933e4d59505af4
Wiktor <syrotinvictor@gmail.com>
2018-10-17 15:26:15 +0200

    page-list with currencies. firebase conf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  resourceUrl = 'http://www.apilayer.net/api/live?access_key=b5306030542bc99e9bfced57f6b6bf0c';
+  list: {name, value}[] = [];
+  waluta = 'USD';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.http.get<{quotes}>(this.resourceUrl, {observe: 'response' })
+      .subscribe(res => {
+        Object.keys(res.body.quotes).forEach((item) => {
+            this.list.push({name: item, value: (res.body.quotes)[item]});
+        });
+      });
+  }
+
 }
